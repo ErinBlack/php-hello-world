e1642aed7d85141534a5e8456f14968243c77445
Erin Black <erinblackdesign@gmail.com>
2017-10-26 19:12:10 -0500

    added heading and comment code

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<h1>My First PHP Page</h1>
 <?php
+// This is a single line comment in PHP
+
+/*
+This is a multiple line comment
+for using larger text
+
+*/
 echo "My first PHP script!";
 ?>
 
